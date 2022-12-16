8b456c3fce1bcd4397ddc0c3a4beef6d46bddc39
Akbar Adji Herlambang <akbaradjiherlambang@gmail.com>
2022-12-16 05:17:41 -1200

    add case 3

--- Program.cs
+++ Program.cs
@@ -127,6 +127,11 @@ namespace CircularQueues_CSharp
                             q.remove();
                         }
                         break;
+                    case '3':
+                        {
+                            q.display();
+                        }
+                        break;
                 }
             }
         }
