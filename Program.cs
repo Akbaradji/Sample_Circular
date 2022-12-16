f610af56075c6c36944e769b21cac3a0c976ee1e
Akbar Adji Herlambang <akbaradjiherlambang@gmail.com>
2022-12-16 05:21:17 -1200

    finish

--- Program.cs
+++ Program.cs
@@ -104,41 +104,48 @@ namespace CircularQueues_CSharp
             char ch;
             while (true)
             {
-                Console.WriteLine("Menu");
-                Console.WriteLine("1. Implement Insert Operation");
-                Console.WriteLine("2. Implement Delete Operation");
-                Console.WriteLine("3. Display Values");
-                Console.WriteLine("4. Exit");
-                Console.WriteLine("\nEnter Your Choice (1-4): ");
-                ch = Convert.ToChar(Console.ReadLine());
-                Console.WriteLine();
-                switch (ch)
+                try
                 {
-                    case '1':
-                        {
-                            Console.Write("Enter a number: ");
-                            int num = Convert.ToInt32(System.Console.ReadLine());
-                            Console.WriteLine();
-                            q.insert(num);
-                        }
-                        break;
-                    case '2':
-                        {
-                            q.remove();
-                        }
-                        break;
-                    case '3':
-                        {
-                            q.display();
-                        }
-                        break;
-                    case '4':
-                        return;
-                    default:
-                        {
-                            Console.WriteLine("Invalid Option!!");
-                        }
-                        break;
+                    Console.WriteLine("Menu");
+                    Console.WriteLine("1. Implement Insert Operation");
+                    Console.WriteLine("2. Implement Delete Operation");
+                    Console.WriteLine("3. Display Values");
+                    Console.WriteLine("4. Exit");
+                    Console.WriteLine("\nEnter Your Choice (1-4): ");
+                    ch = Convert.ToChar(Console.ReadLine());
+                    Console.WriteLine();
+                    switch (ch)
+                    {
+                        case '1':
+                            {
+                                Console.Write("Enter a number: ");
+                                int num = Convert.ToInt32(System.Console.ReadLine());
+                                Console.WriteLine();
+                                q.insert(num);
+                            }
+                            break;
+                        case '2':
+                            {
+                                q.remove();
+                            }
+                            break;
+                        case '3':
+                            {
+                                q.display();
+                            }
+                            break;
+                        case '4':
+                            return;
+                        default:
+                            {
+                                Console.WriteLine("Invalid Option!!");
+                            }
+                            break;
+                    }
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Check for the values Entered. ");
                 }
             }
         }
